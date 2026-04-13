3d84cfa9d2eacc33c5bb5fb92ce4654ef75cca6d
alejandropintado028-pixel <alejandropintado028@gmail.com>
2026-04-13 22:49:05 +0200

    Update Casilla.cpp

--- Casilla.cpp
+++ Casilla.cpp
@@ -2,32 +2,23 @@
 
 Casilla::Casilla(int v, bool i) {
 	valor = v;
-	esInicial = i;
-	ocupado = false;
+	inicial = i;
 }
-
 bool Casilla::es_inicial() const {
-	return esInicial;
+	return inicial;
 }
-
 bool Casilla::esta_ocupada() const {
-	return ocupado == true;
+	return valor != 0;
 }
-
 bool Casilla::esta_libre() const {
-	return ocupado == false;
+	return valor == 0;
 }
-
 int Casilla::tiene_valor() const {
 	return valor;
 }
-
 void Casilla::poner_valor(int v) {
-	valor = v;
-	ocupado = true;
+	if(!es_inicial()) valor = v;
 }
-
 void Casilla::borrar_valor() {
-	valor = 0;
-	ocupado = false;
+	if (!es_inicial()) valor = 0;
 }
